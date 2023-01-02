9bc8fd934eb05c25a12e19496f6cf8b430076173
SAK74 <sergijks@gmail.com>
2023-01-02 18:15:23 +0100

    feat: deploy to gh-pages

--- Async Race/webpack.config.cjs
+++ Async Race/webpack.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "build/Async-Race"),
   },
   mode: "development",
   plugins: [new MiniCssPlugin()],
