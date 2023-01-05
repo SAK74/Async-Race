b72e0c82a7c3dcfb9dc5453f5d9016dfc5dcd52a
SAK74 <sergijks@gmail.com>
2023-01-05 22:35:59 +0100

    style: reduce size of car & winners page styling

--- Async Race/src/components/car/car.js
+++ Async Race/src/components/car/car.js
@@ -10,7 +10,7 @@ export default function car(color, size = 'normal') {
   const carSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   carSVG.setAttributes({
     viewBox: '0 0 1000 1000',
-    width: `${size === 'normal' ? '120' : '60'}`,
+    width: `${size === 'normal' ? '100' : '60'}`,
   });
   carSVG.innerHTML = content;
   carSVG.querySelector('path:first-of-type').style.fill = color;
